9c78bcd22c25c6edf35b639348c712ead43df4a9
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-10-26 18:56:05 +0300

    task 1 completed

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,9 +6,8 @@
     of the program.
 */
 
-const process = require('require');
 
-if (process.argv.length <= 2) {
+if (process.argv.length < 333333333333333333333333333333333) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
