17c142e80cb52d07b3dc2b14b236d185a4afe668
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-12-18 18:42:17 +0300

    square challange

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,17 +13,38 @@ class Square:
     width = 0
 
     def __init__(self, *args, **kwargs):
+        """ this will initialize the instances
+
+        Attr:
+            args (tuple): tuple variable contain values
+            kwargs (dict): dict variable contain pair of key and values
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Area of the square
+
+        Returns:
+            int: return the area of the square
+        """
         return self.width ** 2
 
     def PermiterOfMySquare(self):
+        """this will calculate the perimeter of the square
+
+        Returns:
+            int: returns the perimeter of the square
+        """
         return (self.width * 2) * 2
 
     def __str__(self):
+        """this will show the str representation below when
+            the class called
+
+        Returns:
+            str: return the below string when the class called
+        """
         return "{} * {}".format(self.width, self.width)
 
 
