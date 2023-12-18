0f9ebb8b8d1fb1c461371b6217cf0e1d628aba1d
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-12-18 18:53:17 +0300

    user code fixed

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
+this User class will store the user email
 """
 
-class User():
-    """ Documentation """
 
-    def __init__(self):
-        """ Documentation """
-        self.__email = None
+class User():
+    """ This user class will store the email of the user"""
+
+    def __init__(self, email=None):
+        """ This is method will initialize the email value"""
+        self.__email = email
+
+    @property
+    def email(self):
+        """ this is getter  method for the email instance """
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ this method will set the private instance email
+
+        Attr:
+            value (any): email of the user
+        """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
+
 if __name__ == "__main__":
 
     u = User()
