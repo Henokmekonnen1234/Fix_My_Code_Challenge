ec456cf14daf1145a32a5e15168da43e6cafc621
Henokmekonnen1234 <henokmac2425@gmail.com>
2023-12-18 17:39:28 +0300

    square challange

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+"""
+In this class the area and perimeter will be callculated
+"""
 
-class square():
-    
+class Square:
+    """ in this class area and perimeter will be callculated
+
+    Atributes:
+        width (int): is the length of the square
+    """
     width = 0
-    height = 0
-
-    
+        
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width ** 2
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 2) * 2
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{} * {}".format(self.width, self.width)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
